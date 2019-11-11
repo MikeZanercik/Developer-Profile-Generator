fea27f3c59e07b78e0065b4720f77eb877a466a1
Mike Zanercik <Michael.zanercik@gmail.com>
2019-11-11 15:53:04 -0500

    switched everything to generate MD instead of HTML

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ inquirer.prompt([
     .then(function (answers) {
         axios.get("https://api.github.com/users/" + answers.username)
             .then(function (response) {                
-                fs.writeFile("myPage.html", generateHTML({
+                fs.writeFile("myPage.md", generateMD({
                     name: response.data.login,
                     favColor: answers.color,
                     imgURL: response.data.avatar_url,
@@ -37,46 +37,34 @@ inquirer.prompt([
                     following: response.data.following,
                 }), function(err){
                     if(err) throw err;
-                    console.log("HTML generated!")
+                    console.log("MD generated!")
                 })
             });
     })
 
 
-function generateHTML(data) {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    
-    <body>
-    
-        <div class="container">
-            <h2 style="color:${data.favColor}"> ${data.name} </h2>
-    
-            <hr>
-    
-            <img src="${data.imgURL}" alt="profile image" />
-    
-            <a href="${data.repoLink}">
-            <p>Repo: ${data.repoLink} </p>
-            </a> 
-            <p>Location:  ${data.location}  </p>  
-            <p>Bio:  ${data.bio}  </p>  
-            <p>Public Repos:  ${data.repos}  </p>  
-            <p>Followers:  ${data.followers}  </p>  
-            <p>Following:  ${data.following}  </p>  
+function generateMD(data) {
+    return `## <h1 style="color:${data.favColor}">${data.name}</h1>
 
 
-            
-        </div>
+# ![UserProfileImage](${data.imgURL})
     
-    </body>
     
-    </html>`
+    
+Repo: <a href="${data.repoLink}">${data.name}'s GitHub Profile</a>.
+    
+        
+    Location:  ${data.location}    
+    
+    
+    Bio:  ${data.bio} 
+    
+    
+    Public Repos:  ${data.repos} 
+    
+    
+    Followers:  ${data.followers}  
+    
+    
+    Following:  ${data.following} `
 }
